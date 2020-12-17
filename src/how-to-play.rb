b770ff5100d2421c0f346c97cf49d329717a4d8c
irah rosete <irah.rosete@outlook.com>
2020-12-18 00:34:26 +1100

    updated how to play instructions

--- src/how-to-play.rb
+++ src/how-to-play.rb
@@ -1,17 +1,25 @@
 puts "\n\e[32mHow to Play\e[0m\n\n"
-# sleep 2
-puts "Collect as much food to feed the hungry snake 🐍."
-# sleep 3
-puts "\nUse the ⬅️  ➡️  ⬆️  ⬇️  arrow keys on your keyboard to maneuver the snake."
-# sleep 3
-puts "\nThe more food you collect,"
-# sleep 1.5
+sleep 1.5
+puts "Collect as much food to feed the hungry snake 🐍.\n\n"
+sleep 2
+puts "Use these keys on your keyboard to maneuver the snake:\n\n"
+sleep 0.5
+puts "\e[44m\e[37m w \e[0m UP"
+sleep 0.5
+puts "\e[44m\e[37m a \e[0m LEFT"
+sleep 0.5
+puts "\e[44m\e[37m s \e[0m DOWN"
+sleep 0.5
+puts "\e[44m\e[37m d \e[0m RIGHT\n\n"
+sleep 2
+puts "The more food you collect,"
+sleep 1.5
 puts "the longer the snake becomes,"
-# sleep 1.5
-puts "the higher your score!"
-# sleep 3
-puts "\nMake sure not to collide the snake to the walls or its own body."
-# sleep 3
-puts "Otherwise, Game Over. 🛑"
-# sleep 3
-puts "\nHave fun!\n\n"
+sleep 1.5
+puts "the higher your score!\n\n"
+sleep 3
+puts "Make sure not to collide the snake to the walls or its own body."
+sleep 3
+puts "Otherwise, Game Over. 🛑\n\n"
+sleep 3
+puts "Have fun!\n\n"
